adadd793f562368cfd1d939c903ae2d6adb85b16
Alsuhaibi <alsuhaibiabdulrahman@gmail.com>
2022-07-21 09:35:37 +0300

    saving the files

---
+++ public/js/app.js
@@ -0,0 +1,13 @@
+//variable types
+
+const name = 'Abdulrahman'
+let age = 30
+const gender = 1
+const isSelected = true;
+const lastName = undefined;
+const country = null
+
+let val;
+
+
+console.log(typeof val + ` The age is ${age} and the name is ${name}`);
